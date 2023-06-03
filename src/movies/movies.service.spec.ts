cb89733d28a1ed8cbb0422e58ebf5c1d70f37a89
dahye <dhjin1@naver.com>
2023-06-03 19:50:00 +0900

    unit test

--- src/movies/movies.service.spec.ts
+++ src/movies/movies.service.spec.ts
@@ -14,6 +14,10 @@ describe('MoviesService', () => {
     service = module.get<MoviesService>(MoviesService);
   });
 
+  // afterAll(()=>{
+    //주로 테스트 환경을 청소하거나 해체하는 코드 포함. DB 청소 등 
+  // })
+
   it('should be defined', () => { //이게 테스트임
     expect(service).toBeDefined();
   });
@@ -51,5 +55,59 @@ describe('MoviesService', () => {
       }
     })
   })
+  
+  describe("deleteOne",()=>{
+    it("deletes a movie", ()=>{
+       service.create({
+        title:"Test Movie",
+        genres: ['test'],
+        year:2000,
+      });
+      const allMovies= service.getAll().length;
+      service.deleteOne(1)
+      const afterDelete=service.getAll().length;
 
+      expect(afterDelete).toBeLessThan(allMovies);
+    });
+    it("should return a 404", ()=>{
+      try{
+        service.deleteOne(999)
+      }catch(e){
+        expect(e).toBeInstanceOf(NotFoundException);
+      }
+    });
+  });
+  
+  describe("create", ()=>{
+    it("should create a movie", ()=>{
+      const beforeCreate = service.getAll().length;
+      service.create({
+          title:"Test Movie",
+          genres: ['test'],
+          year:2000,
+      });
+      const afterCreate = service.getAll().length;
+      expect(afterCreate).toBeGreaterThan(beforeCreate);
+    });
+  });
+
+  describe("update",()=>{
+    it("should update a movie", ()=>{
+      service.create({
+        title:"Test Movie",
+        genres: ['test'],
+        year:2000,
+     });
+     service.update(1, {title: 'Updated Test'});
+     const movie = service.getOne(1);
+     expect(movie.title).toEqual('Updated Test');
+    });
+    it("should throw a NotFoundExcepion", ()=>{
+      try{
+        service.update(999, {});
+      }catch(e){
+        expect(e).toBeInstanceOf(NotFoundException);
+      }
+    });
+  })
 });
